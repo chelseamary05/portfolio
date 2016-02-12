3a235f8c18f9d0170c2d74a3bac106b58d96cc84
chelseamary05@gmail.com <chelsea.mary05@gmail.com>
2016-02-12 13:43:44 -0500

    Signed-off-by: chelseamary05@gmail.com <chelsea.mary05@gmail.com>

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Smith Designs Portfolio' });
+});
+
+/* about page*/
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Smith Designs' });
+});
+
+/*photography page*/
+router.get('/photography', function(req, res, next) {
+  res.render('photography', { title: 'Photograhy by Smith Designs' });
+});
+
+/*web work page*/
+ router.get('/web', function(req, res, next) {
+  res.render('web', { title: 'Web Development Work by Smith Designs' });
+});
+
+/*digital art page*/
+router.get('/art', function(req, res, next) {
+  res.render('art', { title: 'Digital Art by Smith Designs' });
+});
+
+/*contact page*/
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us!' });
+});
+
+module.exports = router;
